f32a931140a0bd95165ae7d3106111250b5d1d1b
Jokiie <celia.codere63@gmail.com>
2021-08-01 19:43:24 -0500

    boss bags fixes

--- Items/Boss/ManoTreasureBag.cs
+++ Items/Boss/ManoTreasureBag.cs
@@ -1,3 +1,4 @@
+using System;
 using Terraria;
 using Terraria.ModLoader;
 using Terraria.ID;
@@ -10,11 +11,7 @@ namespace TerraStory.Items.Boss
 {
 	
 	public class ManoTreasureBag: ModItem
-	{
-		
-		public override int BossBagNPC => mod.NPCType("Mano");
-		
-		
+	{	
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Mano Treasure Bag");
@@ -30,7 +27,9 @@ namespace TerraStory.Items.Boss
 			item.rare = ItemRarityID.Cyan;
 			item.expert = true;
 		}
-		
+
+		public override int BossBagNPC => mod.NPCType("Mano");
+
 		public override bool CanRightClick() 
 		{
 			return true;
@@ -38,21 +37,35 @@ namespace TerraStory.Items.Boss
 		
 		public override void OpenBossBag(Player player)
 		{
-			player.QuickSpawnItem(ItemID.SilverCoin, Main.rand.Next(10, 30));
-			player.QuickSpawnItem(ItemID.LesserHealingPotion, Main.rand.Next(1, 5));
-			player.QuickSpawnItem(ItemID.LesserManaPotion, Main.rand.Next(1, 3));
-			player.QuickSpawnItem(ModContent.ItemType<BundleOfMesos>(), Main.rand.Next(0, 3));
-			player.QuickSpawnItem(ModContent.ItemType<RainbowColoredSnailShell>(), 1);
-
 			int choice = Main.rand.Next(4);
 			if (choice == 0)
+			{
 				player.QuickSpawnItem(ModContent.ItemType<Subi>(), Main.rand.Next(50, 100));
+			}
 			if (choice == 1)
+			{
 				player.QuickSpawnItem(ModContent.ItemType<NoviceBomb>(), Main.rand.Next(50, 100));
+			}
 			if (choice == 2)
+			{
 				player.QuickSpawnItem(ModContent.ItemType<Bullet>(), Main.rand.Next(50, 100));
+			}
 			if (choice == 3)
+			{
 				player.QuickSpawnItem(ModContent.ItemType<CopperArrow>(), Main.rand.Next(50, 100));
+			}
+
+			player.QuickSpawnItem(ItemID.SilverCoin, Main.rand.Next(10, 30));
+
+			player.QuickSpawnItem(ItemID.LesserHealingPotion, Main.rand.Next(1, 5));
+
+			player.QuickSpawnItem(ItemID.LesserManaPotion, Main.rand.Next(1, 3));
+
+			player.QuickSpawnItem(mod.ItemType("BundleOfMesos"), Main.rand.Next(1, 3));
+
+			player.QuickSpawnItem(mod.ItemType("RainbowColoredSnailShell"));
+
+
 		}
 	}
 }
